c96828faef533b59e9e64af92c39611a397e397b
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-01 12:43:08 +0530

    add arithmeticOperators

---
+++ js/arithmeticOperators.js
@@ -0,0 +1,37 @@
+// + addition
+// - subtarction
+// * multiplication
+// ** exponential
+// % modulus
+// -- decrement
+// ++ increment
+// +=
+// -+
+// *=
+// /=
+
+const x = 6
+const y = "7"
+console.log(x+y)
+
+const a = 6
+const b = true
+console.log(a+b)
+
+const c = "123"
+const d = 2
+console.log(c*d)
+
+const e = "123"
+const f = 2
+console.log(c/d)
+
+const g = "123"
+const h = 2
+console.log(c-d)
+
+const i = "hello"
+const j = 2
+console.log(i-j)
+
+// NaN - not a number
