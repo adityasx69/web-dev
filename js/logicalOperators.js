ac5ff86b929524eae1b5a1b10f99c0dad7d94aa4
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-01 12:53:12 +0530

    add logicalOperators

---
+++ js/logicalOperators.js
@@ -0,0 +1,14 @@
+// &&
+// ||
+// !
+console.log(true && false)
+console.log(true || false)
+console.log(!(true || false))
+
+console.log("hello" || true)
+console.log("" || true)
+console.log("" || false)
+console.log("" || "hello")
+
+console.log(true && "hello")
+console.log(false && "hello")
