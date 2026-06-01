442629ff82b1cd76605c70f00aaf90439ec5f093
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-01 14:31:09 +0530

    add switch:

---
+++ js/switch.js
@@ -0,0 +1,13 @@
+const value = 3;
+
+switch(value){
+    case 3:
+        console.log("3 is cool")
+        break;
+    case 4:
+        console.log("3 is not cool")
+        break;
+    default:
+        console.log("okay")
+        break;
+}
