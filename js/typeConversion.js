92c582a0b85632394ec0def8b2cc71c8de62dfbd
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-01 12:43:44 +0530

    add typeConversion

---
+++ js/typeConversion.js
@@ -0,0 +1,25 @@
+const x = "1234"
+const y = 2
+console.log(Number(x)+y)
+
+const a = "1234.5px"
+const b = 2
+console.log(parseInt(a)+b)
+
+const c = "1234.5px"
+const d = 2
+console.log(parseFloat(c)+d)
+
+console.log(Number(true) + d)
+
+const e = 1234
+const f = 2
+console.log(String(e)+f)
+
+const g = 1234
+const h = 2
+console.log(x + "")
+
+const i = "1234"
+const j = 2
+console.log(x.toString() + y)
