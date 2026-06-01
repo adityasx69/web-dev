35f42642249bddbbaf9984d59e933263b9886288
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-01 12:43:23 +0530

    add blockVsFunctionScoped

---
+++ js/blockVsFunctionScoped.js
@@ -0,0 +1,16 @@
+if(true){
+    var x = 3
+}
+
+console.log(x)
+
+function test(){
+    console.log(t)
+    var t = "test"
+    console.log(t)
+}
+
+test()
+
+// let and const are block scoped
+// var is function scoped
