9ce6aa7c75056fcb6c0e9d43fd0eeabcdbf1e852
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-01 13:00:35 +0530

    add conditionals

---
+++ js/conditionals.js
@@ -0,0 +1,15 @@
+if(false){
+    console.log("hello world")
+} else if(false){
+    console.log("")
+} else{
+    console.log("false")
+}
+
+if(false)
+    console.log("hello")
+else 
+    console.log("run")
+
+const cond = 2 < 3 ? "okay cool" : "no"
+console.log(cond)
