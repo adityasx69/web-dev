b46eb10b1cde833f26b76b2d60a59266dd95b186
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-06 15:18:32 +0530

    add arrow-function

---
+++ js-for-react/arrow-function.js
@@ -0,0 +1,15 @@
+function doSomething(){
+
+}
+
+const doSomething1 = () => {
+
+}
+
+// buttons example for react
+
+<button>
+    onClick =  {() => {
+        console.log("hello world");
+    }}
+</button>
