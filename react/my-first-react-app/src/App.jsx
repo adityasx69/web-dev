6e35572fcd972eab7395ba11b2392cd68b482f63
Aditya Saraswat <saraswataditya664@gmail.com>
2026-06-07 22:45:23 +0530

    Add App component

---
+++ react/my-first-react-app/src/App.jsx
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react'
+import Search from "./components/search.jsx";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ${API_KEY}'
+    }
+}
+
+const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+
+    },[]);
+
+    return (
+        <main>
+            <div className="pattern"/>
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
+                </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            </div>
+        </main>
+    )
+}
+export default App
